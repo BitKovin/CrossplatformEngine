9606758b572cc34d3ad547ff0fa9487d5fb5dff0
BitKovin <39824091+BitKovin@users.noreply.github.com>
2021-04-12 00:12:49 +0300

    Improved collision accuracy

--- Engine/Engine/Collision.cs
+++ Engine/Engine/Collision.cs
@@ -12,14 +12,14 @@ namespace Engine
         public Point size;
         public Entity owner;
 
-        const int Acuracy = 500;
+        const int Accuracy = 500;
 
         public static bool MakeCollionTest(Collision col1, Collision col2)
         {
-            Point pos1 = new Point((int)(col1.position.X* Acuracy), (int)(col1.position.Y * Acuracy));
-            Point pos2 = new Point((int)(col2.position.X * Acuracy), (int)(col2.position.Y * Acuracy));
-            Rectangle Col1 = new Rectangle(new Point(pos1.X, pos1.Y), new Point(col1.size.X* Acuracy, col1.size.Y * Acuracy));
-            Rectangle Col2 = new Rectangle(new Point(pos2.X, pos2.Y), new Point(col2.size.X * Acuracy, col2.size.Y * Acuracy));
+            Point pos1 = new Point((int)(col1.position.X* Accuracy), (int)(col1.position.Y * Accuracy));
+            Point pos2 = new Point((int)(col2.position.X * Accuracy), (int)(col2.position.Y * Accuracy));
+            Rectangle Col1 = new Rectangle(new Point(pos1.X, pos1.Y), new Point(col1.size.X* Accuracy, col1.size.Y * Accuracy));
+            Rectangle Col2 = new Rectangle(new Point(pos2.X, pos2.Y), new Point(col2.size.X * Accuracy, col2.size.Y * Accuracy));
 
             return Col1.Intersects(Col2);
 
